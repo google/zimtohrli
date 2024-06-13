8668e92c448e097cb8ef317ab58020125a9b486a
Jyrki Alakuijala <jyrki@google.com>
2024-06-13 11:41:41 +0200

    special casing norm=2 for speedup
    Before:
    
    |Score type |MSE               |Min score         |Max score         |Mean score        |
    |-----------|------------------|------------------|------------------|------------------|
    |Zimtohrli  |0.101641872045228 |0.563959418943879 |0.762245848804069 |0.686398636984063 |
    |ViSQOL     |0.115330916105424 |0.520833375452983 |0.801480831107469 |0.675101633981268 |
    |2f         |0.129541391104905 |0.484687555319526 |0.797475783883375 |0.661870345773127 |
    |PESQ       |0.147425552045669 |0.342342966279351 |0.841271127756762 |0.647128996775172 |
    |CDPAM      |0.153471222942756 |0.441558428344727 |0.728779141125759 |0.620699318941738 |
    |PARLAQ     |0.185057687192323 |0.445261140223642 |0.784370761057963 |0.587162756572532 |
    |AQUA       |0.223207996944378 |0.331645933512413 |0.739286336419790 |0.547804951221731 |
    |PEAQB      |0.225217321572038 |0.278744167467764 |0.851011116004117 |0.553935720513487 |
    |DPAM       |0.315810440183130 |0.186717781679534 |0.690564701717118 |0.460415212267967 |
    |WARP-Q     |0.339686211572685 |0.067600137543649 |0.777119464646524 |0.475793617709890 |
    |GVPMOS     |0.412937133868407 |0.006851162794410 |0.783946603687895 |0.412912222208318 |
    
    real	44m52.908s # not indicative as the terminal was blocked by
    another window and consequently blocking the computation on my desktop
    
    user	748m34.686s
    sys	219m0.831s
    
    After:
    
    |Score type |MSE               |Min score         |Max score         |Mean score        |
    |-----------|------------------|------------------|------------------|------------------|
    |Zimtohrli  |0.101642192840456 |0.563959418943879 |0.762245848804069 |0.686398039616168 |
    |ViSQOL     |0.115330916105424 |0.520833375452983 |0.801480831107469 |0.675101633981268 |
    |2f         |0.129541391104905 |0.484687555319526 |0.797475783883375 |0.661870345773127 |
    |PESQ       |0.147425552045669 |0.342342966279351 |0.841271127756762 |0.647128996775172 |
    |CDPAM      |0.153471222942756 |0.441558428344727 |0.728779141125759 |0.620699318941738 |
    |PARLAQ     |0.185057687192323 |0.445261140223642 |0.784370761057963 |0.587162756572532 |
    |AQUA       |0.223207996944378 |0.331645933512413 |0.739286336419790 |0.547804951221731 |
    |PEAQB      |0.225217321572038 |0.278744167467764 |0.851011116004117 |0.553935720513487 |
    |DPAM       |0.315810440183130 |0.186717781679534 |0.690564701717118 |0.460415212267967 |
    |WARP-Q     |0.339686211572685 |0.067600137543649 |0.777119464646524 |0.475793617709890 |
    |GVPMOS     |0.412937133868407 |0.006851162794410 |0.783946603687895 |0.412912222208318 |
    
    real	13m52.362s
    user	625m20.070s
    sys	279m59.803s
    
    17 % reduction in user time
    
    some more speedup on the gammatone filter side

--- cpp/zimt/dtw.cc
+++ cpp/zimt/dtw.cc
@@ -71,11 +71,31 @@ float HwyDeltaNorm(hwy::Span<const float> span_a,
   CHECK_EQ(span_a.size(), span_b.size());
 
   double sum = 0;
+<<<<<<< HEAD
   for (size_t index = 0; index < span_a.size(); index += Lanes(d)) {
     const Vec delta =
         Sub(Load(d, span_a.data() + index), Load(d, span_b.data() + index));
     const Vec square = Mul(delta, delta);
     sum += static_cast<double>(ReduceSum(d, square));
+=======
+  if (order == 2.0) {
+    // Faster special case without Exp/Log for order == 2.0, the usual case.
+    for (size_t index = 0; index < span_a.size(); index += Lanes(d)) {
+      const Vec delta =
+          Sub(Load(d, span_a.data() + index), Load(d, span_b.data() + index));
+      const Vec pows = Mul(delta, delta);
+      sum += static_cast<double>(ReduceSum(d, pows));
+    }
+    sum /= max * max;
+  } else {
+    for (size_t index = 0; index < span_a.size(); index += Lanes(d)) {
+      const Vec delta =
+          Sub(Load(d, span_a.data() + index), Load(d, span_b.data() + index));
+      const Vec downscaled_values = Mul(delta, max_reciprocal);
+      const Vec pows = Exp(d, Mul(order_vec, Log(d, downscaled_values)));
+      sum += static_cast<double>(ReduceSum(d, pows));
+    }
+>>>>>>> 0117bd7 (special casing norm=2 for speedup)
   }
   return static_cast<float>(std::sqrt(sum));
 }
