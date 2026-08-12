2fb6a1ad9da6e377d5972afe97782571d0defe65
Sami Boukortt <sboukortt@google.com>
2026-08-12 17:48:42 +0200

    Make Python optional

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -13,16 +13,18 @@ target_link_libraries(zimtohrli_test gtest sndfile gmock_main benchmark absl::st
 target_compile_definitions(zimtohrli_test PRIVATE CMAKE_CURRENT_SOURCE_DIR=${CMAKE_CURRENT_SOURCE_DIR})
 gtest_discover_tests(zimtohrli_test)
 
-set(python3_VENV_DIR ${CMAKE_CURRENT_BINARY_DIR}/venv)
-set(python3_VENV ${python3_VENV_DIR}/bin/python3)
-configure_file(${CMAKE_CURRENT_SOURCE_DIR}/cpp/zimt/pyohrli.py ${CMAKE_CURRENT_BINARY_DIR}/pyohrli.py COPYONLY)
-configure_file(${CMAKE_CURRENT_SOURCE_DIR}/cpp/zimt/pyohrli_test.py ${CMAKE_CURRENT_BINARY_DIR}/pyohrli_test.py COPYONLY)
-add_test(NAME zimtohrli_pyohrli_test
-    COMMAND sh -c "${Python3_EXECUTABLE} -m venv ${python3_VENV_DIR} &&
-        ${python3_VENV} -m pip install jax jaxlib numpy scipy &&
-        ${python3_VENV} pyohrli_test.py"
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-)
+if (Python3_FOUND)
+  set(python3_VENV_DIR ${CMAKE_CURRENT_BINARY_DIR}/venv)
+  set(python3_VENV ${python3_VENV_DIR}/bin/python3)
+  configure_file(${CMAKE_CURRENT_SOURCE_DIR}/cpp/zimt/pyohrli.py ${CMAKE_CURRENT_BINARY_DIR}/pyohrli.py COPYONLY)
+  configure_file(${CMAKE_CURRENT_SOURCE_DIR}/cpp/zimt/pyohrli_test.py ${CMAKE_CURRENT_BINARY_DIR}/pyohrli_test.py COPYONLY)
+  add_test(NAME zimtohrli_pyohrli_test
+      COMMAND sh -c "${Python3_EXECUTABLE} -m venv ${python3_VENV_DIR} &&
+          ${python3_VENV} -m pip install jax jaxlib numpy scipy &&
+          ${python3_VENV} pyohrli_test.py"
+      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+  )
+endif ()
 
 add_test(NAME zimtohrli_go_test
     COMMAND go test ./...
